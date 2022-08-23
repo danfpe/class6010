4cece66b511f9b1c2273c6aa591bcc2332572372
danfa-peng <danfa_peng@apple.com>
2022-08-24 00:45:43 +0800

    add data type

--- HelloWorld/HelloWorld/main.cpp
+++ HelloWorld/HelloWorld/main.cpp
@@ -10,8 +10,18 @@ using namespace std;
 int main(int argc, const char * argv[]) {
     // insert code here...
     int a = 2, b = 3;
+    string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+    float myNumber = 4.55;
+    double myDoubleNumber = 5.89;
+    char myLetter = 'D';
+    bool myBoolean = true;
     cout << "Hello, World!\n";
     cout << "Make the World better! " << "GOOGLE" << endl;
     cout << a << " " << b << endl;
+    cout << cars[0] << endl;
+    cout << myNumber << endl;
+    cout << myDoubleNumber << endl;
+    cout << myLetter << endl;
+    cout << myBoolean << endl;
     return 0;
 }
