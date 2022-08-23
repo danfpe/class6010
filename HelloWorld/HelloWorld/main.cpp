ebc4b40b153d09ff12f3463d3ef78db2c0382780
danfa-peng <danfa_peng@apple.com>
2022-08-23 14:17:08 +0800

    the first push

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by Peng, Danfa  on 2022/8/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    std::cout << "Make the World better!\n";
+    return 0;
+}
